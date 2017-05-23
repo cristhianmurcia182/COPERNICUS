5c20737fdb2cc8ef6fcc34e114712713b92e1e64
jeilones <jeisolon.espi@gmail.com>
2017-05-24 01:57:00 +0200

    Added process management procedure
    Storing process status in JSON files.
    
    It is necessary to create the "ProcessStatus" and specify its path in
    the python script, the folder must containg the "empty" three json
    files:
    processing.json
    processed.json
    errors.json

--- Snap graph/batchExecution.py
+++ Snap graph/batchExecution.py
@@ -6,7 +6,32 @@ import subprocess
 
 processXMLPath = "D:\ifgi\Copernicus\SnapAutomation\\batch\\"
 inputPath = "D:\ifgi\Copernicus\SnapAutomation\Input\\"
-outputPath = "D:\ifgi\Copernicus\SnapAutomation\Onput\\"
+outputPath = "D:\ifgi\Copernicus\SnapAutomation\Output\\"
+processStatusPath = "D:\ifgi\Copernicus\SnapAutomation\ProcessStatus\\"
+
+
+def writeProcessStatusFiles():
+    processStatusJSon = processStatusPath + "processing.json"
+    with open(processStatusJSon, 'wb') as outfile:
+        json.dump({}, outfile)
+
+    processStatusJSon = processStatusPath + "processed.json"
+    with open(processStatusJSon, 'wb') as outfile:
+        json.dump({}, outfile)
+
+    processStatusJSon = processStatusPath + "errors.json"
+    with open(processStatusJSon, 'wb') as outfile:
+        json.dump({}, outfile)
+
+
+# writeProcessStatusFiles()
+
+
+def enum(**enums):
+    return type('Enum', (), enums)
+
+
+ProcessStatus = enum(PROCESSING='PROCESSING', PROCESSED='PROCESSED', ERROR='ERROR')
 
 
 def readFiles():
@@ -14,15 +39,46 @@ def readFiles():
     for filename in listdir:
         startTime = time.time()
         thread.start_new_thread(processImage, (filename,))
-        pid = getPID(filename)
-        metadata = json.dumps({
-            "name": filename,
+        pid = getProcessID(filename)
+        processStatusJSon = processStatusPath + "processing.json"
+        data = readProcessStatusInJson(processStatusJSon)
+        jsonData = {
             "pid": pid,
-            "status: ": "STATUS",
-            "starttime": time.time()
-        })
+            "status: ": ProcessStatus.PROCESSING,
+            "starttime": startTime
+        }
 
-        print metadata
+        addProcessStatusDataToJson(filename, data, jsonData)
+        writeProcessStatusInJson(processStatusJSon, data)
+
+
+def addProcessStatusDataToJson(filename, data, jsonData):
+    if filename not in data:
+        statusData = {}
+
+        for key in jsonData:
+            statusData[key] = jsonData[key]
+
+        data[filename] = statusData
+
+        print data
+
+
+def deleteElementJson(jsonPath, data, filename):
+    if filename in data:
+        del data[filename]
+
+
+def writeProcessStatusInJson(jsonPath, data):
+    with open(jsonPath, 'w') as outfile:
+        json.dump(data, outfile)
+
+
+def readProcessStatusInJson(jsonPath):
+    with open(jsonPath, 'r') as outfile:
+        print "JSON Status"
+        data = json.load(outfile)
+    return data
 
 
 def processImage(filename):
@@ -30,14 +86,41 @@ def processImage(filename):
     command = "gpt %sPreProcess.xml -Pfilename=\"%s%s\" -Poutputfilename=\"%s%s.dim\"" % (
         processXMLPath, inputPath, filename, outputPath, filename)
     print command
-    os.system(command)
+
+    try:
+        os.system(command)
+    except:
+        print "Error: unable to start thread"
+        # here is necessary to add the error management procedure
+
+    endTime = time.time()
+
+    processStatusJSon = processStatusPath + "processing.json"
+    data = readProcessStatusInJson(processStatusJSon)
+    processingStatus = data[filename]
+
+    deleteElementJson(processStatusJSon, data, filename)
+    writeProcessStatusInJson(processStatusJSon, data)
+
+    processStatusJSon = processStatusPath + "processed.json"
+    data = readProcessStatusInJson(processStatusJSon)
+
+    jsonData = {
+        "pid": processingStatus["pid"],
+        "status: ": ProcessStatus.PROCESSED,
+        "starttime": processingStatus["starttime"],
+        "endtime": endTime
+    }
+    addProcessStatusDataToJson(filename, data, jsonData)
+    writeProcessStatusInJson(processStatusJSon, data)
+
     print "End time: %s" % time.ctime(time.time())
 
 
-def getPID(filename):
-    delay = 4
+def getProcessID(filename):
+    delay = 2
     time.sleep(delay)
-    print "getPID"
+
     wmic_cmd = """wmic process where "name='gpt.exe'" get commandline,processid"""  # or name='snap64.exe'
     wmic_prc = subprocess.Popen(wmic_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     wmic_out, wmic_err = wmic_prc.communicate()
@@ -51,4 +134,21 @@ def getPID(filename):
     return None
 
 
-readFiles()
+try:
+    thread.start_new_thread(readFiles, ())
+except:
+    print "Error: unable to start thread"
+
+
+def isProcessing():
+    time.sleep(10)
+    processStatusJSon = processStatusPath + "processing.json"
+    data = readProcessStatusInJson(processStatusJSon)
+    for processingFilename in data:
+        return True
+
+    return False
+
+
+while isProcessing():
+    pass
